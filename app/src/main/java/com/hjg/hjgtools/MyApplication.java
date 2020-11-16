77baaa369f253a75398a5dc4a25d42fe9bf1d35e
jimmy <jiguo.hou@dragonsoftbravo.com>
2020-11-16 11:47:12 +0800

    log开关控制。

--- app/src/main/java/com/hjg/hjgtools/MyApplication.java
+++ app/src/main/java/com/hjg/hjgtools/MyApplication.java
@@ -3,11 +3,13 @@ package com.hjg.hjgtools;
 import android.app.Application;
 
 import com.hjg.base.util.Utils;
+import com.hjg.base.util.log.androidlog.L;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Utils.init(this);
+        L.getLogConfig().configAllowLog(true);
     }
 }
