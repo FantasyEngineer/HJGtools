8962c05ef22c4f3e641153fbba964673ff6496f5
jimmy <jiguo.hou@dragonsoftbravo.com>
2021-01-26 15:09:09 +0800

    单位时间内按了多少次按钮

--- app/src/main/java/com/hjg/hjgtools/activity/widget/ButtonActivity.java
+++ app/src/main/java/com/hjg/hjgtools/activity/widget/ButtonActivity.java
@@ -37,64 +37,30 @@ public class ButtonActivity extends HJGDatabindingBaseActivity<ActivityButtonBin
     protected void initViewAction() {
 
 
-//        RxView.clicks(databinding.btnDuringTimeClickCount).buffer(5, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<List<Object>>() {
-//            @Override
-//            public void accept(List<Object> objects) throws Exception {
-//                D.showShort("5s内共点击了" + objects.size() + "次");
-//            }
-//        });
-//
-//        RxView.clicks(databinding.btnPreventMulClick).throttleFirst(5, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Object>() {
-//            @Override
-//            public void accept(Object o) throws Exception {
-//                D.showShort("操作函数接收到第一个数据后，5s内不再接受数据");
-//            }
-//        });
-//
-//        RxView.clicks(databinding.btnPreventMulClick2).throttleLatest(5, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Object>() {
-//            @Override
-//            public void accept(Object o) throws Exception {
-//                D.showShort("接收到操作函数5s内的最后一个数据" + (++count));
-//            }
-//        });
-//
-
-
-        Observable.create(new ObservableOnSubscribe<Integer>() {
+        RxView.clicks(databinding.btnDuringTimeClickCount).buffer(5, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<List<Object>>() {
             @Override
-            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
-                databinding.btnPreventMulClick2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        emitter.onNext(++count);
-                    }
-                });
-            }
-        }).throttleLatest(5, TimeUnit.SECONDS).subscribe(new Consumer<Integer>() {
-            @Override
-            public void accept(Integer o) throws Exception {
-
-                L.d("????" + o);
+            public void accept(List<Object> objects) throws Exception {
+                if (objects.size() > 0) {
+                    D.showShort("5s内共点击了" + objects.size() + "次");
+                }
             }
         });
+
+        RxView.clicks(databinding.btnPreventMulClick).throttleFirst(5, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(Object o) throws Exception {
+                D.showShort("操作函数接收到第一个数据后，5s内不再接受数据");
+            }
+        });
+
+        RxView.clicks(databinding.btnPreventMulClick2).throttleLatest(5, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(Object o) throws Exception {
+                D.showShort("接收到操作函数5s内的最后一个数据" + (++count));
+            }
+        });
+
+
     }
 
-    public void duringTimeClickCount(View view) {
-
-        D.showShort("duringTimeClickCount");
-
-    }
-
-    public void preventMulClickNeedFirst(View view) {
-        D.showShort("preventMulClickNeedFirst");
-
-
-    }
-//
-//    public void preventMulClickNeedLast(View view) {
-//
-//        D.showShort("preventMulClickNeedLast");
-//
-//
-//    }
 }
