9d2feaf51af6258a218a3a9dafc8bed055dbd401
jimmy <jiguo.hou@dragonsoftbravo.com>
2021-02-02 09:50:12 +0800

    edittext

---
+++ Base/src/main/java/com/hjg/base/view/SmartEditText.java
@@ -0,0 +1,19 @@
+package com.hjg.base.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+    public class SmartEditText extends androidx.appcompat.widget.AppCompatEditText {
+    public SmartEditText(Context context) {
+        super(context);
+    }
+
+    public SmartEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SmartEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
